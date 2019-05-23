79b3e3807bd95ef7dffbd9bdb8f2fe83224f5129
mskim <ms0329.kim@gmail.com>
2019-05-23 10:29:03 +0900

    요구사항 2

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,14 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,7 +24,7 @@ public class RequestHandler extends Thread {
 				connection.getPort());
 
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			String url = null;
+			String url = null, requestPath = null;
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String line = buffer.readLine();
 
@@ -31,16 +34,28 @@ public class RequestHandler extends Thread {
 				}
 
 				String[] tokens = line.split(" ");
+				url = tokens[1];
 
-				if ("/".equals(tokens[1])) {
-					url = "/index.html";
+				if ("/".equals(url)) {
+					requestPath = "/index.html";
+				} else if (url.contains("/user/create")) {
+					if ("GET".equals(tokens[0])) {
+						int index = url.indexOf("?");
+						requestPath = "/user/form.html";
+
+						if (index > -1) {
+							String params = url.substring(index+1);
+							createUser(params);
+							requestPath = "/index.html";
+						}
+					}
 				}
 
 				line = buffer.readLine();
 			}
 
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+			byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
@@ -66,5 +81,11 @@ public class RequestHandler extends Thread {
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
+	}
+
+	private void createUser(String params) {
+		Map<String, String> map = HttpRequestUtils.parseQueryString(params);
+		User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+		System.out.println(user.toString());
 	}
 }
