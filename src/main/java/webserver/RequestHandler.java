6ff68eeb6ea49ce61dae29030a29c7ab7e2b1b83
mskim <ms0329.kim@gmail.com>
2019-05-23 16:37:00 +0900

    was-step1-bad version : 요구사항 1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -27,87 +27,24 @@ public class RequestHandler extends Thread {
 				connection.getPort());
 
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			String url = null, requestPath = null, type = null;
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-			DataOutputStream dos = new DataOutputStream(out);
 			String line = buffer.readLine();
-			int count = 0, contentLength = 0;
-			boolean acceptYn = false;
+			log.debug("request line : {}", line);
 
-			while (!"".equals(line)) {
-				if (line == null) {
-					return;
-				}
+			if (line == null) {
+				return;
+			}
 
-				String[] tokens = line.split(" ");
-				if (count++ == 0) {
-					type = tokens[0];
-					url = tokens[1];
+			String[] tokens = line.split(" ");
 
-					if (url.contains(".css") || url.contains(".js")) {
-						acceptYn = true;
-						requestPath = url;
-					}
-				}
-
-				if (url.contains("html")) {
-					requestPath = url;
-				} else {
-					if ("/".equals(url)) {
-						requestPath = "/index.html";
-					} else if (url.contains("/user/create")) {
-						if ("GET".equals(type)) {
-							int index = url.indexOf("?");
-							requestPath = "/user/form.html";
-
-							if (index > -1) {
-								String params = url.substring(index+1);
-								createUser(params);
-								response302Header(dos);
-							}
-						}
-					} else if (url.contains("list")) {
-						if (tokens[0].contains("Cookie")) {
-							Map<String, String> map = HttpRequestUtils.parseQueryString(tokens[1]);
-
-							if (!Boolean.parseBoolean(map.get("logined"))) {
-								requestPath = "/user/login.html";
-							} else {
-								requestPath = "/user/list.html";
-							}
-						}
-					}
-				}
-
-				if (tokens[0].contains("Content-Length")) {
-					contentLength = Integer.parseInt(tokens[1]);
-				}
-
+			while (!line.equals("")) {
 				line = buffer.readLine();
+				log.debug("header: {}", line);
 			}
 
-			String loginYn = null;
-			if (contentLength > 0) {
-
-				if (url.contains("create")) {
-					createUser(IOUtils.readData(buffer, contentLength));
-					response302Header(dos);
-				} else if (url.contains("login")) {
-					User user = findUser(IOUtils.readData(buffer, contentLength));
-
-					if (user == null) {
-						requestPath = "/login_failed.html";
-						loginYn = "logined=false";
-					} else {
-						requestPath = "/";
-						loginYn = "logined=true";
-					}
-				}
-
-			}
-
-			byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
-			response200Header(dos, body.length, loginYn, acceptYn);
+			DataOutputStream dos = new DataOutputStream(out);
+			byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+			response200Header(dos, body.length);
  			responseBody(dos, body);
 		} catch (IOException e) {
 			log.error(e.getMessage());
@@ -125,56 +62,14 @@ public class RequestHandler extends Thread {
 		}
 	}
 
-	private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String cookieYn, Boolean acceptYn) {
-		try {
-			dos.writeBytes("HTTP/1.1 200 OK \r\n");
-
-			if (acceptYn) {
-				dos.writeBytes("Content-Type: text/css\r\n");
-			} else {
-				dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-			}
-
-			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-
-			if (cookieYn != null) {
-				dos.writeBytes("Cookie: " + cookieYn + "\r\n");
-			}
-
-
-			dos.writeBytes("\r\n");
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
-
-	private void response302Header(DataOutputStream dos) {
-		try {
-			dos.writeBytes("HTTP/1.1 302 Found \r\n");
-			dos.writeBytes("Location: /index.html");
-			dos.writeBytes("\r\n");
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
-
 	private void responseBody(DataOutputStream dos, byte[] body) {
 		try {
 			dos.write(body, 0, body.length);
+			dos.writeBytes("\r\n");
 			dos.flush();
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
 	}
 
-	private User findUser(String params) {
-		Map<String, String> map = HttpRequestUtils.parseQueryString(params);
-		return DataBase.findUserById(map.get("userId"));
-	}
-
-	private void createUser(String params) {
-		Map<String, String> map = HttpRequestUtils.parseQueryString(params);
-		User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
-		DataBase.addUser(user);
-	}
 }
