0b90bf787d00c9e853ff373bd279dcd971189281
mskim <ms0329.kim@gmail.com>
2019-05-24 15:38:18 +0900

    step2 : 요구사항 2 - HttpResponse

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -1,38 +1,69 @@
 package http;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class HttpResponse {
-
+	private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
 	private DataOutputStream dos;
+	private Map<String, String> headers = new HashMap<>();
 
 	public HttpResponse(OutputStream out) {
 		this.dos = new DataOutputStream(out);
 	}
 
-	public void addHeader(String type, String content) {
+	public void addHeader(String key, String value) {
+		headers.put(key, value);
+	}
+
+	public void forward(String url) throws IOException {
+		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+
+		if (url.endsWith(".css")) {
+			headers.put("Content-Type", "text/css");
+		} else if (url.endsWith(".js")) {
+			headers.put("Content-Type", "application/javascript");
+		} else {
+			headers.put("Content-Type", "text/html;charset=utf-8");
+		}
+		headers.put("Content-Length", body.length + "");
+		response200Header(body.length);
+		responseBody(body);
+	}
+
+	public void forwardBody(String body) {
+		byte[] contents = body.getBytes();
+		headers.put("Content-Type", "text/html;charset=utf-8");
+		headers.put("Content-Length", contents.length + "");
+		response200Header(contents.length);
+		responseBody(contents);
+	}
+
+	public void sendRedirect(String redirectUrl) {
 		try {
-			dos.writeBytes(type + ": " + content + " \r\n");
+			dos.writeBytes("HTTP/1.1 302 Found \r\n");
+			processHeaders();
+			dos.writeBytes("Location: " + redirectUrl + " \r\n");
+			dos.writeBytes("\r\n");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void forward(String url) throws IOException {
-		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-		response200Header(body.length);
-		responseBody(body);
-	}
 
 	public void response200Header(int lengthOfBodyContent) {
 		try {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
-			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			processHeaders();
 			dos.writeBytes("\r\n");
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -49,19 +80,16 @@ public class HttpResponse {
 		}
 	}
 
-	public void sendRedirect(String location) {
+
+	public void processHeaders() {
 		try {
-			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
-			dos.writeBytes("Location: " + location + " \r\n");
-			dos.writeBytes("\r\n");
+			Set<String> keys = headers.keySet();
+			for (String key : keys) {
+				dos.writeBytes(key + ": " + headers.get(key) + "\r\n");
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-	}
-
-	public void processHeaders() {
-
 	}
 
 }
